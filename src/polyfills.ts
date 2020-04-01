a090940d0851a8be865ec7ca85f2c78dcf843911
Daphne Smit <daphnesmit1986@gmail.com>
2020-04-01 14:29:55 +0200

    feat(wretch): implement wretch instead of custom http class

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,8 @@
 /**
  * Add global polyfills for application
  * Next.js adds corejs (babel-polyfill), the URL api and fetch by default so you dont have to include those
+ * @see {@link https://www.npmjs.com/package/intersection-observer|intersection-observer}
+ * @see {@link https://www.npmjs.com/package/abortcontroller-polyfill|abortcontroller-polyfill}
  */
 import 'intersection-observer'
-// import './a11y'
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch'
