5fd9a4f92a50d8400c1309219c38f816a09bb022
Daphne Smit <daphnesmit1986@gmail.com>
2020-04-08 13:51:25 +0200

    fix(locales): locale path

--- server/i18n.ts
+++ server/i18n.ts
@@ -5,7 +5,7 @@ import nl from 'date-fns/locale/nl'
 import NextI18Next from 'next-i18next'
 
 const NextI18NextInstance = new NextI18Next({
-  localePath: (process as any).browser ? 'locales' : 'public/locales',
+  localePath: 'public/locales',
   ns: ['common', 'yup'],
   defaultNS: 'common',
   defaultLanguage: 'nl',
